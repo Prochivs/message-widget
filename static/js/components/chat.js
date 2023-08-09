797caa4335c181b5331229caf90182a34fab7124
Pro Chivs <prochivia@gmail.com>
2023-08-09 18:03:44 +0200

    update

---
+++ static/js/components/chat.js
@@ -0,0 +1,188 @@
+/**
+ * scroll to the bottom of the chats after new message has been added to chat
+ */
+const converter = new showdown.Converter();
+function scrollToBottomOfResults() {
+  const terminalResultsDiv = document.getElementById("chats");
+  terminalResultsDiv.scrollTop = terminalResultsDiv.scrollHeight;
+}
+
+/**
+ * Set user response on the chat screen
+ * @param {String} message user message
+ */
+function setUserResponse(message) {
+  const user_response = `<img class="userAvatar" src='./static/img/userAvatar.jpg'><p class="userMsg">${message} </p><div class="clearfix"></div>`;
+  $(user_response).appendTo(".chats").show("slow");
+
+  $(".usrInput").val("");
+  scrollToBottomOfResults();
+  showBotTyping();
+  $(".suggestions").remove();
+}
+
+/**
+ * returns formatted bot response
+ * @param {String} text bot message response's text
+ *
+ */
+function getBotResponse(text) {
+  botResponse = `<img class="botAvatar" src="./static/img/sara_avatar.png"/><span class="botMsg">${text}</span><div class="clearfix"></div>`;
+  return botResponse;
+}
+
+/**
+ * renders bot response on to the chat screen
+ * @param {Array} response json array containing different types of bot response
+ *
+ * for more info: `https://rasa.com/docs/rasa/connectors/your-own-website#request-and-response-format`
+ */
+function setBotResponse(response) {
+  // renders bot response after 500 milliseconds
+  setTimeout(() => {
+    hideBotTyping();
+    if (response.length > 1) {
+      // if there is no response from Rasa, send  fallback message to the user
+      const fallbackMsg = response;
+
+      const BotResponse = `<img class="botAvatar" src="./static/img/sara_avatar.png"/><p class="botMsg">${fallbackMsg}</p><div class="clearfix"></div>`;
+
+      $(BotResponse).appendTo(".chats").hide().fadeIn(1000);
+      scrollToBottomOfResults();
+    } else {
+      // if there is no response from Rasa, send  fallback message to the user
+      const fallbackMsg = "I am facing some issues, please try again later!!!";
+
+      const BotResponse = `<img class="botAvatar" src="./static/img/sara_avatar.png"/><p class="botMsg">${fallbackMsg}</p><div class="clearfix"></div>`;
+
+      $(BotResponse).appendTo(".chats").hide().fadeIn(1000);
+      scrollToBottomOfResults();
+    }
+
+    $(".usrInput").focus();
+  }, 500);
+}
+
+/**
+ * sends the user message to the rasa server,
+ * @param {String} message user message
+ */
+async function send(message) {
+  if ("WebSocket" in window) {
+    //  alert("WebSocket is supported by your Browser!");
+
+    // Let us open a web socket
+    var ws = new WebSocket("ws://54.160.88.29/websocket");
+
+    ws.onopen = function () {
+      // Web Socket is connected, send data using send()
+      ws.send(message);
+      // alert("Message is sent...");
+    };
+
+    ws.onmessage = function (evt) {
+      var botResponse = evt.data;
+      // alert("Message is received...");
+      setBotResponse(botResponse);
+    };
+
+    ws.onclose = function () {
+      // websocket is closed.
+      alert("Connection is closed...");
+    };
+  } else {
+    // The browser doesn't support WebSocket
+    alert("WebSocket NOT supported by your Browser!");
+  }
+
+}
+
+
+/**
+ * clears the conversation from the chat screen
+ * & sends the `/resart` event to the Rasa server
+ */
+function restartConversation() {
+  $("#userInput").prop("disabled", true);
+  // destroy the existing chart
+  $(".collapsible").remove();
+
+  if (typeof chatChart !== "undefined") {
+    chatChart.destroy();
+  }
+
+  $(".chart-container").remove();
+  if (typeof modalChart !== "undefined") {
+    modalChart.destroy();
+  }
+  $(".chats").html("");
+  $(".usrInput").val("");
+  send("/restart");
+}
+// triggers restartConversation function.
+$("#restart").click(() => {
+  restartConversation();
+});
+
+/**
+ * if user hits enter or send button
+ * */
+$(".usrInput").on("keyup keypress", (e) => {
+  const keyCode = e.keyCode || e.which;
+
+  const text = $(".usrInput").val();
+  if (keyCode === 13) {
+    if (text === "" || $.trim(text) === "") {
+      e.preventDefault();
+      return false;
+    }
+    // destroy the existing chart, if yu are not using charts, then comment the below lines
+    $(".collapsible").remove();
+    $(".dropDownMsg").remove();
+    if (typeof chatChart !== "undefined") {
+      chatChart.destroy();
+    }
+
+    $(".chart-container").remove();
+    if (typeof modalChart !== "undefined") {
+      modalChart.destroy();
+    }
+
+    $("#paginated_cards").remove();
+    $(".suggestions").remove();
+    $(".quickReplies").remove();
+    $(".usrInput").blur();
+    setUserResponse(text);
+    send(text);
+    e.preventDefault();
+    return false;
+  }
+  return true;
+});
+
+$("#sendButton").on("click", (e) => {
+  const text = $(".usrInput").val();
+  if (text === "" || $.trim(text) === "") {
+    e.preventDefault();
+    return false;
+  }
+  // destroy the existing chart
+  if (typeof chatChart !== "undefined") {
+    chatChart.destroy();
+  }
+
+  $(".chart-container").remove();
+  if (typeof modalChart !== "undefined") {
+    modalChart.destroy();
+  }
+
+  $(".suggestions").remove();
+  $("#paginated_cards").remove();
+  $(".quickReplies").remove();
+  $(".usrInput").blur();
+  $(".dropDownMsg").remove();
+  setUserResponse(text);
+  send(text);
+  e.preventDefault();
+  return false;
+});
