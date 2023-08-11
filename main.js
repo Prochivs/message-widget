190bc0b2cacb4b21f8c3c2281ad99fc13c6cafba
Pro Chivs <prochivia@gmail.com>
2023-08-11 17:40:55 +0200

    updates

--- main.js
+++ main.js
@@ -1,266 +1,115 @@
-import { CLOSE_ICON, MESSAGE_ICON, styles } from "./assets.js";
+// Create the widget container
+const widgetContainer = document.createElement("div");
 
-class MessageWidget {
-  constructor(position = "bottom-right") {
-    this.position = this.getPosition(position);
-    this.open = false;
-    this.initialize();
-    this.injectStyles();
-  }
+// Create a custom HTML element
+const customHtmlElement = document.createElement("div");
+customHtmlElement.innerHTML = `
+<div class="container">
+<!-- Modal for rendering the charts, declare this if you want to render charts, 
+   else you remove the modal -->
 
-  position = "";
-  open = false;
-  widgetContent = null;
+<!--chatbot widget -->
+<div class="widget">
+  <div class="chat_header">
+    <!--Add the name of the bot here -->
+    <span class="chat_header_title">Xavier Ai</span>
+    <span class="dropdown-trigger" href="#" data-target="dropdown1">
+      <i class="material-icons"> more_vert </i>
+    </span>
 
-  getPosition(position) {
-    const [vertical, horizontal] = position.split("-");
-    return {
-      [vertical]: "30px",
-      [horizontal]: "30px",
-    };
-  }
-
-  async initialize() {
-    /**
-     * Create and append a div element to the document body
-     */
-    const container = document.createElement("div");
-    container.style.position = "fixed";
-    Object.keys(this.position).forEach(
-      (key) => (container.style[key] = this.position[key])
-    );
-    document.body.appendChild(container);
-
-    /**
-     * Create a button element and give it a class of button__container
-     */
-    const buttonContainer = document.createElement("button");
-    buttonContainer.classList.add("button__container");
-
-    /**
-     * Create a span element for the widget icon, give it a class of `widget__icon`, and update its innerHTML property to an icon that would serve as the widget icon.
-     */
-    const widgetIconElement = document.createElement("span");
-    widgetIconElement.innerHTML = MESSAGE_ICON;
-    widgetIconElement.classList.add("widget__icon");
-    this.widgetIcon = widgetIconElement;
-
-    /**
-     * Create a span element for the close icon, give it a class of `widget__icon` and `widget__hidden` which would be removed whenever the widget is closed, and update its innerHTML property to an icon that would serve as the widget icon during that state.
-     */
-    const closeIconElement = document.createElement("span");
-    closeIconElement.innerHTML = CLOSE_ICON;
-    closeIconElement.classList.add("widget__icon", "widget__hidden");
-    this.closeIcon = closeIconElement;
-
-    /**
-     * Append both icons created to the button element and add a `click` event listener on the button to toggle the widget open and close.
-     */
-    buttonContainer.appendChild(this.widgetIcon);
-    buttonContainer.appendChild(this.closeIcon);
-    buttonContainer.addEventListener("click", this.toggleOpen.bind(this));
-
-    /**
-     * Create a container for the widget and add the following classes:- `widget__hidden`, `widget__container`
-     */
-    this.widgetContainer = document.createElement("div");
-    this.widgetContainer.classList.add("widget__hidden", "widget__container");
-
-    /**
-     * Invoke the `createWidget()` method
-     */
-    this.createWidgetContent();
-
-    /**
-     * Append the widget's content and the button to the container
-     */
-    container.appendChild(this.widgetContainer);
-    container.appendChild(buttonContainer);
-  }
-
-  createWidgetContent() {
-    this.widgetContainer.innerHTML = `
-    <div class="container">
-    <!-- Modal for rendering the charts, declare this if you want to render charts, 
-       else you remove the modal -->
-      
-    <link
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      rel="stylesheet"
-    />
-    
-
-   
-    <link
-      href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
-      rel="stylesheet"
-    />
-
-    <!--Import Font Awesome Icon Font-->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-      integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0="
-      crossorigin="anonymous"
-    />
-
-    <!--Import materialize.css-->
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="static/css/materialize.min.css"
-    />
-
-    <!--Main css-->
-    <link rel="stylesheet" type="text/css" href="static/css/style.css" />
-     
-    <!--chatbot widget -->
-    <div class="widget">
-      <div class="chat_header">
-        <!--Add the name of the bot here -->
-        <span class="chat_header_title">Xavier Ai</span>
-        <span class="dropdown-trigger" href="#" data-target="dropdown1">
-          <i class="material-icons"> more_vert </i>
-        </span>
-
-        <!-- Dropdown menu-->
-        <ul id="dropdown1" class="dropdown-content">
-          <li><a href="#" id="clear">Clear</a></li>
-          <li><a href="#" id="restart">Restart</a></li>
-          <li><a href="#" id="close">Close</a></li>
-        </ul>
-      </div>
-
-      <!--Chatbot contents goes here -->
-      <div class="chats" id="chats">
-        <div class="clearfix"></div>
-      </div>
-
-      <!--keypad for user to type the message -->
-      <div class="keypad">
-        <textarea
-          id="userInput"
-          placeholder="Type a message..."
-          class="usrInput"
-        ></textarea>
-        <div id="sendButton">
-          <i class="fa fa-paper-plane" aria-hidden="true"></i>
-        </div>
-      </div>
-    </div>
-
-    <!--bot profile-->
-    <div class="profile_div" id="profile_div">
-      <img class="imgProfile" src="static/img/chat.png" />
-    </div>
-   
+    <!-- Dropdown menu-->
+    <ul id="dropdown1" class="dropdown-content">
+      <li><a href="#" id="clear">Clear</a></li>
+      <li><a href="#" id="restart">Restart</a></li>
+      <li><a href="#" id="close">Close</a></li>
+    </ul>
   </div>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script
-    type="text/javascript"
-    src="static/js/lib/materialize.min.js"
-  ></script>
-  <script src="static/js/lib/uuid.min.js"></script>
-  <script>
-  
-/* module for importing other js files */
-function include(file) {
-  const script = document.createElement('script');
-  script.src = file;
-  script.type = 'text/javascript';
-  script.defer = true;
+  <!--Chatbot contents goes here -->
+  <div class="chats" id="chats">
+    <div class="clearfix"></div>
+  </div>
 
-  document.getElementsByTagName('head').item(0).appendChild(script);
+  <!--keypad for user to type the message -->
+  <div class="keypad">
+    <textarea
+      id="userInput"
+      placeholder="Type a message..."
+      class="usrInput"
+    ></textarea>
+    <div id="sendButton">
+      <i class="fa fa-paper-plane" aria-hidden="true"></i>
+    </div>
+  </div>
+</div>
+
+<!--bot profile-->
+<div class="profile_div" id="profile_div">
+  <img class="imgProfile" src="static/img/chat.png" />
+</div>
+
+
+</div>
+`;
+
+// Append custom HTML, quote elements, and the generate button
+widgetContainer.appendChild(customHtmlElement);
+
+// Append the container to the body
+document.body.appendChild(widgetContainer);
+
+// Add event listener to the generate button
+
+// Load external scripts
+function loadScript(src, callback) {
+  const script = document.createElement("script");
+  script.src = src;
+  script.onload = callback;
+  document.body.appendChild(script);
 }
 
+// Load external CSS file
+function loadCSS(href) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = href;
+  document.head.appendChild(link);
+}
 
-// Bot pop-up intro
-document.addEventListener("DOMContentLoaded", () => {
-  const elemsTap = document.querySelector(".tap-target");
-  // eslint-disable-next-line no-undef
-  const instancesTap = M.TapTarget.init(elemsTap, {});
-  instancesTap.open();
-  setTimeout(() => {
-    instancesTap.close();
-  }, 4000);
+// Load additional scripts
+loadScript(
+  "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js",
+  () => {
+    console.log("Additional script  0 loaded.");
+  }
+);
+
+// Load external script tags
+loadScript("static/js/lib/materialize.min.js", () => {
+  console.log("External script 1 loaded.");
 });
 
-/* import components */
-include('./static/js/components/index.js');
-
-window.addEventListener('load', () => {
-  // initialization
-  $(document).ready(() => {
-    // Bot pop-up intro
-    $("div").removeClass("tap-target-origin");
-
-    // drop down menu for close, restart conversation & clear the chats.
-    $(".dropdown-trigger").dropdown();
-
-    // initiate the modal for displaying the charts,
-    // if you dont have charts, then you comment the below line
-    $(".modal").modal();
-
-    // enable this if u have configured the bot to start the conversation.
-    // showBotTyping();
-    // $("#userInput").prop('disabled', true);
-
-    // if you want the bot to start the conversation
-    // customActionTrigger();
-  });
-  // Toggle the chatbot screen
-  $("#profile_div").click(() => {
-    $(".profile_div").toggle();
-    $(".widget").toggle();
-  });
-
-  // clear function to clear the chat contents of the widget.
-  $("#clear").click(() => {
-    $(".chats").fadeOut("normal", () => {
-      $(".chats").html("");
-      $(".chats").fadeIn();
-    });
-  });
-
-  // close function to close the widget.
-  $("#close").click(() => {
-    $(".profile_div").toggle();
-    $(".widget").toggle();
-    scrollToBottomOfResults();
-  });
+loadScript("static/js/lib/uuid.min.js", () => {
+  console.log("External script 2 loaded.");
 });
-  
-  </script>
-  <script type="text/javascript" src="static/js/lib/chart.min.js"></script>
-  <script type="text/javascript" src="static/js/lib/showdown.min.js"></script>
 
-    `;
-  }
+loadScript("/static/js/script.js", () => {
+  console.log("External script 3 loaded.");
+});
 
-  injectStyles() {
-    const styleTag = document.createElement("style");
-    styleTag.innerHTML = styles.replace(/^\s+|\n/gm, "");
-    document.head.appendChild(styleTag);
-  }
+loadScript("static/js/lib/chart.min.js", () => {
+  console.log("External script 4 loaded.");
+});
 
-  toggleOpen() {
-    this.open = !this.open;
-    if (this.open) {
-      this.widgetIcon.classList.add("widget__hidden");
-      this.closeIcon.classList.remove("widget__hidden");
-      this.widgetContainer.classList.remove("widget__hidden");
-    } else {
-      this.createWidgetContent();
-      this.widgetIcon.classList.remove("widget__hidden");
-      this.closeIcon.classList.add("widget__hidden");
-      this.widgetContainer.classList.add("widget__hidden");
-    }
-  }
-}
+loadScript("static/js/lib/showdown.min.js", () => {
+  console.log("External script 5 loaded.");
+});
 
-function initializeWidget() {
-  return new MessageWidget();
-}
-
-initializeWidget();
+// Load external CSS files
+loadCSS("static/css/materialize.min.css");
+loadCSS("static/css/style.css");
+loadCSS(
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+);
+loadCSS("https://fonts.googleapis.com/css2?family=Lato&display=swap");
+loadCSS("https://fonts.googleapis.com/icon?family=Material+Icons");
