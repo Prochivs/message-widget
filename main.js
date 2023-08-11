f2f3eff7d60c4d491aade8a3b7b7dc175e80c6bd
Prochivs <75859835+Prochivs@users.noreply.github.com>
2023-08-11 19:52:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-// import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
 
+setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
 document.querySelector('#app').innerHTML = `
   <div>
    
